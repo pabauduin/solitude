ed60bea113ecdde19fe8d64ea09fd5199b397e8d
rg <gonera.raphael@gmail.com>
2019-08-20 18:46:41 +0200

    new & edit user profil

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
 gem 'devise'
+gem 'pundit'
 
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_092302) do
+ActiveRecord::Schema.define(version: 2019_08_20_153625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,17 +44,15 @@ ActiveRecord::Schema.define(version: 2019_08_20_092302) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email_address"
+    t.string "email"
     t.string "city"
     t.string "user_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
