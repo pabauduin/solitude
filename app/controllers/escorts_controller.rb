32bc323eeb26ad0a9c420e9f4507fa5aa1b7e858
sara louzai <sara.louzay@gmail.com>
2019-08-20 10:16:21 +0200

    controller escort

--- app/controllers/escorts_controller.rb
+++ app/controllers/escorts_controller.rb
@@ -1,22 +1,39 @@
 class EscortsController < ApplicationController
   def index
+    @escorts = Escort.all
   end
 
   def show
+    @escort = Escort.find(params[:id])
   end
 
   def new
+    @escort = Escort.new
   end
 
   def create
+    @escort = Escort.new(params[:escort_params])
+    if @escort.save
+      redirect_to @escort
+    else
+      render new
+    end
   end
 
   def edit
+    @escort = Escort.find(params[:id])
   end
 
   def update
+    @escort = Escort.find(params[:id])
   end
 
   def delete
+    @escort = Escort.find(params[:id])
+    @escort.destroy
+  end
+
+  def escort_params
+    params.require(:escort).permit(:name, :age, :size, :type, :city, :hair_color)
   end
 end
