260d84f765c123de41a43beb581768e08be5b325
PA Bauduin <bauduinpa@gmail.com>
2019-08-20 17:09:17 +0200

    oops i pushed on master

--- app/controllers/escorts_controller.rb
+++ app/controllers/escorts_controller.rb
@@ -1,13 +1,13 @@
 class EscortsController < ApplicationController
   def index
-    @price_range = params[:search][:price_per_day].split('..').inject { |s,e| s.to_i..e.to_i }
+    # @price_range = params[:search][:price_per_day].split('..').inject { |s,e| s.to_i..e.to_i }
 
     @escorts = Escort.where(city: params[:search][:city].capitalize,
-                            gender: params[:search][:gender].capitalize,
-                            age: params[:search][:age],
-                            size: params[:search][:size],
-                            available_dates: params[:search][:available_dates],
-                            price_per_day: @price_range)
+                            gender: params[:search][:gender].capitalize)
+                            # age: params[:search][:age],
+                            # size: params[:search][:size],
+                            # available_dates: params[:search][:available_dates],
+                            # price_per_day: @price_range)
   end
 
   def show
